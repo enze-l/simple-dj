82d38dc3b7f37a4e548aadf3a9a9bac3d0599298
enze-l <leon.enzenberger@protonmail.com>
2022-04-08 15:55:49 +0200

    Play music file on upload - Remove old styling - Remove old Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [file, setFile] = useState();
+
+    let audioContext: AudioContext;
+    let audioElement: HTMLAudioElement | null;
+
+    useEffect(() => {
+        if (!audioContext) {
+            audioContext = new window.AudioContext;
+            console.log(file)
+        }
+        if (file) {
+            audioElement = new Audio(URL.createObjectURL(file));
+            if (audioElement) {
+                const track = audioContext.createMediaElementSource(audioElement);
+                track.connect(audioContext.destination);
+                audioContext.resume();
+                audioElement.play();
+            }
+        }
+    }, [file])
+
+    const handeFile = (e: any) => {
+        setFile(e.target.files[0]);
+    }
+
+    return (
+        <div className="App">
+            <header className="grid place-items-center h-screen">
+                <input onChange={handeFile} id="audio" type="file" accept="audio/*"/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
