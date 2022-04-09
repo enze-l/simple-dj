b94341541c5ded2aea02a300b1ee285e912ac6e7
enze-l <leon.enzenberger@protonmail.com>
2022-04-09 08:31:02 +0200

    Make Play/Pause-Button react to playing state

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
     const [file, setFile] = useState();
+    const [buttonText, setButtonText] = useState("Play")
 
-    const audioContext = new AudioContext;
-    var audioElement: HTMLAudioElement;
-    var track;
+    const PLAY = "Play";
+    const PAUSE = "Pause";
+
+    const audioElement = useRef<HTMLAudioElement>(new Audio());
 
     useEffect(() => {
         if (file) {
-            audioElement = new Audio(URL.createObjectURL(file));
-            track = audioContext.createMediaElementSource(audioElement);
+            const audioContext = new AudioContext();
+            audioElement.current = new Audio(URL.createObjectURL(file));
+            const track = audioContext.createMediaElementSource(audioElement.current);
             track.connect(audioContext.destination);
             audioContext.resume();
-            audioElement.play();
+            audioElement.current.play();
         }
     }, [file])
 
-    const handleFile = (e: any) => {
-        if(audioElement){
-            audioElement.pause();
+    const handleFileUpload = (e: any) => {
+        if(e.target.files[0]) {
+            audioElement.current.pause();
+            setButtonText(PAUSE)
+            setFile(e.target.files[0]);
         }
-        setFile(e.target.files[0]);
     }
 
     const handlePlayPause = () => {
-        if(audioElement) {
-            if (audioElement.paused) {
-                audioElement.play();
-            } else {
-                audioElement.pause();
-            }
+        if (audioElement.current.paused){
+            audioElement.current.play();
+            setButtonText(PAUSE);
+        } else {
+            audioElement.current.pause();
+            setButtonText(PLAY);
         }
     }
 
     return (
         <div className="grid place-items-center h-screen">
             <div className="grid grid-cols-1">
-                <button onClick={handlePlayPause}>Play/Pause</button>
-                <input onChange={handleFile} id="audio" type="file" accept="audio/*"/>
+                <button onClick={handlePlayPause}>{buttonText}</button>
+                <input onChange={handleFileUpload} id="audio" type="file" accept="audio/*"/>
             </div>
         </div>
     );
