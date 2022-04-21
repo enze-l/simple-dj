53f39aec734cf8e79d123358d8255ac901fe409b
Leon Enzenberger <46045816+enze-l@users.noreply.github.com>
2022-04-21 14:02:45 +0200

    Setup: 7 add linting (#10)
    * Play music file on upload
    - Remove old styling
    - Remove old Content
    
    * Make music pausable
    - Add play/pause button
    - Stop music if new file is submitted
    
    * Remove redundant method
    
    * Make Play/Pause-Button react to playing state
    
    * Exchange Text-Playbutton with icons
    
    * Add linting
    
    * Remove lint warning by utilizing .then
    
    * Add linter workflow
    
    * Fix test by adding text
    
    * Delete workflow name to acoid wf duplicate
    
    * Test deleting lint workflow
    
    * Revert "Test deleting lint workflow"
    
    This reverts commit 1e7e7e51a9317d56f75a62f76df679559067bcc4.
    
    * Try using only one name for steps to avoid duplicate workflows
    
    * Try deleting all names
    
    * Try removing caching of npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import IconButton from '@mui/material/IconButton';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 
 function App() {
+  const [file, setFile] = useState();
+  const [playButton, setPlayButton] = useState(<PlayArrowIcon />);
+
+  const audioElement = useRef<HTMLAudioElement>();
+
+  const playSong = () => {
+    if (audioElement.current) {
+      audioElement.current.play();
+      setPlayButton(<PauseIcon />);
+    }
+  };
+
+  const pauseSong = () => {
+    if (audioElement.current) {
+      audioElement.current.pause();
+      setPlayButton(<PlayArrowIcon />);
+    }
+  };
+
+  useEffect(() => {
+    if (file) {
+      const audioContext = new AudioContext();
+      audioElement.current = new Audio(URL.createObjectURL(file));
+      const track = audioContext.createMediaElementSource(audioElement.current);
+      track.connect(audioContext.destination);
+      audioContext.resume().then(() => playSong());
+    }
+  }, [file]);
+
+  const handleFileUpload = (e: any) => {
+    if (e.target.files[0]) {
+      pauseSong();
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handlePlayPause = () => {
+    if (audioElement.current) {
+      if (audioElement.current.paused) {
+        playSong();
+      } else {
+        pauseSong();
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-items-center h-screen">
+      <div className="">
+        <div className="text-4xl text-center">Simple-Dj</div>
+        <input onChange={handleFileUpload} id="audio" type="file" accept="audio/*" />
+        <IconButton onClick={handlePlayPause}>{playButton}</IconButton>
+      </div>
     </div>
   );
 }
