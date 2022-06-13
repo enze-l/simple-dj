143cf60c27d378dc63f08dc97e1691aff8b49875
enze-l <leon.enzenberger@protonmail.com>
2022-06-13 20:21:21 +0200

    Style befor utilizing dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Slider } from '@mui/material';
+import {
+  createTheme, Slider, ThemeProvider,
+} from '@mui/material';
 import React, { useState } from 'react';
+import { grey } from '@mui/material/colors';
 import SoundControl from './SoundControl';
 import SongListItem from './SongListItem';
 import Waveform from './Visualizer/Waveform';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[900],
+    },
+    secondary: {
+      main: grey[300],
+    },
+  },
+});
 
 function App() {
   const [files, setFiles] = useState<Array<File | undefined>>([undefined]);
@@ -48,77 +62,83 @@ function App() {
   };
 
   return (
-    <div className="flex bg-gray-600">
-      <div className="grow h-screen flex flex-col">
-        <div className="grow grid grid-cols-2 items-center">
-          <SoundControl
-            audioContext={audioContext}
-            setAudioNodes={(node: AudioNode[]) => setAudioNodesOne(node)}
-            volume={2 - volume}
-            playing={onePlaying}
-            file={playerFileOne}
-            togglePlay={() => setTogglePlayerOne(togglePlayerOne + 1)}
-            handlePlayerClose={() => handlePlayerOneSongEnd()}
-          />
-          <SoundControl
-            audioContext={audioContext}
-            setAudioNodes={(node: AudioNode[]) => setAudioNodesTwo(node)}
-            volume={volume}
-            playing={twoPlaying}
-            file={playerFileTwo}
-            togglePlay={() => setTogglePlayerTwo(togglePlayerTwo + 1)}
-            handlePlayerClose={() => handlePlayerTwoSongEnd()}
-          />
-        </div>
-        <div className="h-24 w-2/3 self-center">
-          <Slider
-            value={volume}
-            min={0}
-            max={2}
-            step={0.01}
-            marks={[{ value: 1 }]}
-            track={false}
-            onChange={(e, value) => {
-              if (typeof value === 'number') {
-                setVolume(value);
-              }
-            }}
-          />
-        </div>
-        <div className="h-40">
-          <Waveform
-            toggle={togglePlayerOne}
-            audioContext={audioContext}
-            audioNodes={audioNodesOne}
-            play={(state: boolean) => setOnePlaying(state)}
-            handleSongEnd={() => handlePlayerOneSongEnd()}
-            file={playerFileOne}
-            close={togglePLayerOneClose}
-          />
-        </div>
-        <div className="h-40">
-          <Waveform
-            toggle={togglePlayerTwo}
-            audioContext={audioContext}
-            audioNodes={audioNodesTwo}
-            play={(state: boolean) => setTwoPlaying(state)}
-            handleSongEnd={() => handlePlayerTwoSongEnd()}
-            file={playerFileTwo}
-            close={togglePLayerTwoClose}
-          />
-        </div>
-      </div>
-      <div className="flex-none w-96 bg-gray-700 shadow-xl shadow-gray-800">
-        <input onChange={handleFileUpload} id="audio" type="file" accept="audio/*" />
-        <ul>
-          {files.map((file, index) => (
-            <div onKeyDown={() => handleSongItemClicked(index)} role="button" tabIndex={index} onClick={() => handleSongItemClicked(index)}>
-              <SongListItem file={file} />
+    <ThemeProvider theme={theme}>
+      <div className="flex bg-gray-800 divide-x divide-gray-600">
+        <div className="grow h-screen flex flex-col divide-y divide-gray-600">
+          <div className="grow grid grid-cols-2 items-center">
+            <SoundControl
+              audioContext={audioContext}
+              setAudioNodes={(node: AudioNode[]) => setAudioNodesOne(node)}
+              volume={2 - volume}
+              playing={onePlaying}
+              file={playerFileOne}
+              togglePlay={() => setTogglePlayerOne(togglePlayerOne + 1)}
+              handlePlayerClose={() => handlePlayerOneSongEnd()}
+            />
+            <SoundControl
+              audioContext={audioContext}
+              setAudioNodes={(node: AudioNode[]) => setAudioNodesTwo(node)}
+              volume={volume}
+              playing={twoPlaying}
+              file={playerFileTwo}
+              togglePlay={() => setTogglePlayerTwo(togglePlayerTwo + 1)}
+              handlePlayerClose={() => handlePlayerTwoSongEnd()}
+            />
+          </div>
+          <div className="w-full flex place-content-center">
+            <div className="pb-7 pt-8 w-2/3">
+              <Slider
+                color="secondary"
+                value={volume}
+                min={0}
+                max={2}
+                step={0.01}
+                marks={[{ value: 1 }]}
+                track={false}
+                onChange={(e, value) => {
+                  if (typeof value === 'number') {
+                    setVolume(value);
+                  }
+                }}
+              />
             </div>
-          ))}
-        </ul>
+          </div>
+          <div className="h-40 bg-gray-600">
+            <Waveform
+              toggle={togglePlayerOne}
+              audioContext={audioContext}
+              audioNodes={audioNodesOne}
+              play={(state: boolean) => setOnePlaying(state)}
+              handleSongEnd={() => handlePlayerOneSongEnd()}
+              file={playerFileOne}
+              close={togglePLayerOneClose}
+            />
+          </div>
+          <div className="h-40 bg-gray-600">
+            <Waveform
+              toggle={togglePlayerTwo}
+              audioContext={audioContext}
+              audioNodes={audioNodesTwo}
+              play={(state: boolean) => setTwoPlaying(state)}
+              handleSongEnd={() => handlePlayerTwoSongEnd()}
+              file={playerFileTwo}
+              close={togglePLayerTwoClose}
+            />
+          </div>
+        </div>
+        <div className="flex-none w-96">
+          <input onChange={handleFileUpload} id="audio" type="file" accept="audio/*" />
+          <ul>
+            {files.map((file, index) => (
+              <div onKeyDown={() => handleSongItemClicked(index)} role="button" tabIndex={index} onClick={() => handleSongItemClicked(index)}>
+                <SongListItem file={file} />
+                <hr style={{ borderColor: '#666c75' }} />
+              </div>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/SongListItem.tsx
+++ src/SongListItem.tsx
@@ -5,8 +5,19 @@ export interface AudioPlayerProps {
 }
 
 function SongListItem({ file }: AudioPlayerProps) {
+  const maxLength = 44;
+  let name;
+  if (file) {
+    name = file.name.slice(0, file.name.length - 4);
+    if (file && name.length > maxLength + 4) {
+      name = name.slice(0, maxLength);
+      name = `${name}...`;
+    } else {
+      name = name.slice(0, maxLength + 4);
+    }
+  }
   return (
-    <li>{file?.name}</li>
+    file ? <li className="text-gray-300 p-3">{file ? name : undefined}</li> : <div />
   );
 }
 
